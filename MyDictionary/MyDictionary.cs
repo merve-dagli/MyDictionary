b789945743eb5951425575c23400cf500f5acb35
merve dagli <mmervedagli@gmail.com>
2021-03-18 14:07:38 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<ID, GameName, GamePrice>
+    {
+        public MyDictionary()
+        {
+            liste = new (ID, GameName, GamePrice)[0];
+        }
+        (ID, GameName, GamePrice)[] liste;
+
+        public void Add(ID gameId, GameName gameName, GamePrice gamePrice)
+        {
+            (ID, GameName, GamePrice)[] tempArray = liste;
+            liste = new (ID, GameName, GamePrice)[liste.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                liste[i] = tempArray[i];
+            }
+            liste[liste.Length - 1] = (gameId, gameName, gamePrice);
+        }
+        public int Count
+        {
+            get { return liste.Length; }
+        }
+
+        public void Listing()
+        {
+            Console.WriteLine("-------Oyunlar-------");
+            for (int i = 0; i < liste.Length; i++)
+            {
+                Console.WriteLine(liste[i]);
+            }
+        }
+       
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string, string> games = new MyDictionary<int, string, string>();
+            games.Add(15555, "Ea Pc The Sıms 4 Get To Work", "119 TL");
+            games.Add(15556, "Call Of Duty 4 Modern Warfare PC", "85 TL");
+            games.Add(15557, "Fifa 21 Standard Edition PC Oyun", "509 TL");
+            games.Add(15558, "Star Wars Jedi Fallen Order PC Oyun", "305 TL");
+            Console.WriteLine("Kaç adet oyun vardır? " +games.Count);
+            games.Listing();
+           
+            games.Add(15559, "UK Truck Simulator PC", "37 TL");
+            Console.WriteLine("Kaç adet oyun vardır? " + games.Count);
+            games.Listing();
+         
+
+          
+        }
+      
+    }
+}
